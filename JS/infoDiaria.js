bd246b1268950fccf38c1ca58aa21e0a8b51483d
isaquelopes047 <isaque.lopes047@gmail.com>
2021-10-30 12:24:17 -0300

    add

--- JS/infoDiaria.js
+++ JS/infoDiaria.js
@@ -2,27 +2,48 @@
 let diariaFull = 65.60
 
 const todasAsDiarias = {
-    diariaUm: diariaFull,
+    diaria1: diariaFull,
+    diaria2: diariaFull * 2,
+    diaria3: diariaFull * 3,
+    diaria4: diariaFull * 4,
+    diaria5: diariaFull * 5,
+    diaria6: diariaFull * 6,
+    diaria7: diariaFull * 7,
+    diaria8: diariaFull * 8,
+    diaria9: diariaFull * 9,
+    diaria10: diariaFull * 10,
 };
 
-let textoUm = JSON.stringify(diariaFull);
-let textoDois = JSON.stringify(diariaFull * 2);
+const obj1 = document.querySelector('.infoDiaria1');
+obj1.innerHTML = todasAsDiarias.diaria1;
 
-const mostrar = document.querySelector('.infoDiariaList');
-mostrar.innerHTML = textoDois;
+const obj2 = document.querySelector('.infoDiaria2');
+obj2.innerHTML = todasAsDiarias.diaria2;
+
+const obj3 = document.querySelector('.infoDiaria3');
+obj3.innerHTML = todasAsDiarias.diaria3.toFixed(2);
+
+const obj4 = document.querySelector('.infoDiaria4');
+obj4.innerHTML = todasAsDiarias.diaria4.toFixed(2);
+
+const obj5 = document.querySelector('.infoDiaria5');
+obj5.innerHTML = todasAsDiarias.diaria5.toFixed(2);
+
+const obj6 = document.querySelector('.infoDiaria6');
+obj6.innerHTML = todasAsDiarias.diaria6.toFixed(2);
+
+const obj7 = document.querySelector('.infoDiaria7');
+obj7.innerHTML = todasAsDiarias.diaria7.toFixed(2);
+
+const obj8 = document.querySelector('.infoDiaria8');
+obj8.innerHTML = todasAsDiarias.diaria8.toFixed(2);
+
+const obj9 = document.querySelector('.infoDiaria9');
+obj9.innerHTML = todasAsDiarias.diaria9.toFixed(2);
+
+const obj10 = document.querySelector('.infoDiaria10');
+obj10.innerHTML = todasAsDiarias.diaria10.toFixed(2);
 
 
-const lista = document.querySelector('.infoDiariaList');
-function lista() {
-    // cria um novo elemento div
-    // e dá à ele conteúdo
-    var divNova = document.createElement("div");
-    var conteudoNovo = document.createTextNode("Olá, cumprimentos!");
-    divNova.appendChild(conteudoNovo); //adiciona o nó de texto à nova div criada
-
-    // adiciona o novo elemento criado e seu conteúdo ao DOM
-    var divAtual = document.getElementById("div1");
-    document.body.insertBefore(divNova, divAtual);
-}
 
 
