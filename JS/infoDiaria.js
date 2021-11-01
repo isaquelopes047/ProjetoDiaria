5e2f91946eb7ff9a778fc4ff3c14fad4f234ec03
Isaquelopes91 <isaque.lopes047@gmail.com>
2021-11-01 06:49:39 -0300

    add

--- JS/infoDiaria.js
+++ JS/infoDiaria.js
@@ -1,7 +1,5 @@
-
-let diariaFull = 65.60
-
-const todasAsDiarias = {
+let diariaFull = 65.60;
+const newLocal = {
     diaria1: diariaFull,
     diaria2: diariaFull * 2,
     diaria3: diariaFull * 3,
@@ -13,37 +11,28 @@ const todasAsDiarias = {
     diaria9: diariaFull * 9,
     diaria10: diariaFull * 10,
 };
+const todasAsDiarias = newLocal;
 
-const obj1 = document.querySelector('.infoDiaria1');
-obj1.innerHTML = todasAsDiarias.diaria1;
-
-const obj2 = document.querySelector('.infoDiaria2');
-obj2.innerHTML = todasAsDiarias.diaria2;
-
-const obj3 = document.querySelector('.infoDiaria3');
-obj3.innerHTML = todasAsDiarias.diaria3.toFixed(2);
-
-const obj4 = document.querySelector('.infoDiaria4');
-obj4.innerHTML = todasAsDiarias.diaria4.toFixed(2);
-
-const obj5 = document.querySelector('.infoDiaria5');
-obj5.innerHTML = todasAsDiarias.diaria5.toFixed(2);
-
-const obj6 = document.querySelector('.infoDiaria6');
-obj6.innerHTML = todasAsDiarias.diaria6.toFixed(2);
-
-const obj7 = document.querySelector('.infoDiaria7');
-obj7.innerHTML = todasAsDiarias.diaria7.toFixed(2);
-
-const obj8 = document.querySelector('.infoDiaria8');
-obj8.innerHTML = todasAsDiarias.diaria8.toFixed(2);
-
-const obj9 = document.querySelector('.infoDiaria9');
-obj9.innerHTML = todasAsDiarias.diaria9.toFixed(2);
-
-const obj10 = document.querySelector('.infoDiaria10');
-obj10.innerHTML = todasAsDiarias.diaria10.toFixed(2);
-
-
-
-
+function newFunction() {
+    const obj1 = document.querySelector('.infoDiaria1');
+    obj1.innerHTML = `1 Diaria - ${todasAsDiarias.diaria1.toFixed(2)}`;
+    const obj2 = document.querySelector('.infoDiaria2');
+    obj2.innerHTML = `2 Diaria - ${todasAsDiarias.diaria2.toFixed(2)}`;
+    const obj3 = document.querySelector('.infoDiaria3');
+    obj3.innerHTML = `3 Diaria - ${todasAsDiarias.diaria3.toFixed(2)}`;
+    const obj4 = document.querySelector('.infoDiaria4');
+    obj4.innerHTML = `4 Diaria - ${todasAsDiarias.diaria4.toFixed(2)}`;
+    const obj5 = document.querySelector('.infoDiaria5');
+    obj5.innerHTML = `5 Diaria - ${todasAsDiarias.diaria5.toFixed(2)}`;
+    const obj6 = document.querySelector('.infoDiaria6');
+    obj6.innerHTML = `6 Diaria - ${todasAsDiarias.diaria6.toFixed(2)}`;
+    const obj7 = document.querySelector('.infoDiaria7');
+    obj7.innerHTML = `7 Diaria - ${todasAsDiarias.diaria7.toFixed(2)}`;
+    const obj8 = document.querySelector('.infoDiaria8');
+    obj8.innerHTML = `8 Diaria - ${todasAsDiarias.diaria8.toFixed(2)}`;
+    const obj9 = document.querySelector('.infoDiaria9');
+    obj9.innerHTML = `9 Diaria - ${todasAsDiarias.diaria9.toFixed(2)}`;
+    const obj10 = document.querySelector('.infoDiaria10');
+    obj10.innerHTML = `10 Diaria - ${todasAsDiarias.diaria10.toFixed(2)}`;
+}
+newFunction();
