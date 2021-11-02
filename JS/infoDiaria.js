d8a74f3ed4bd8c84a8248a0af79b47083fe22eb0
isaquelopes047 <isaque.lopes047@gmail.com>
2021-11-02 12:28:30 -0300

    add

--- JS/infoDiaria.js
+++ JS/infoDiaria.js
@@ -15,24 +15,24 @@ const todasAsDiarias = newLocal;
 
 newFunction = () => {
     const obj1 = document.querySelector('.infoDiaria1');
-    obj1.innerHTML = `1 Diaria - ${todasAsDiarias.diaria1.toFixed(2)}`;
+    obj1.innerHTML = `| 1 Diaria - ${todasAsDiarias.diaria1.toFixed(2)}`;
     const obj2 = document.querySelector('.infoDiaria2');
-    obj2.innerHTML = `2 Diaria - ${todasAsDiarias.diaria2.toFixed(2)}`;
+    obj2.innerHTML = `| 2 Diarias - ${todasAsDiarias.diaria2.toFixed(2)}`;
     const obj3 = document.querySelector('.infoDiaria3');
-    obj3.innerHTML = `3 Diaria - ${todasAsDiarias.diaria3.toFixed(2)}`;
+    obj3.innerHTML = `| 3 Diarias - ${todasAsDiarias.diaria3.toFixed(2)}`;
     const obj4 = document.querySelector('.infoDiaria4');
-    obj4.innerHTML = `4 Diaria - ${todasAsDiarias.diaria4.toFixed(2)}`;
+    obj4.innerHTML = `| 4 Diarias - ${todasAsDiarias.diaria4.toFixed(2)}`;
     const obj5 = document.querySelector('.infoDiaria5');
-    obj5.innerHTML = `5 Diaria - ${todasAsDiarias.diaria5.toFixed(2)}`;
+    obj5.innerHTML = `| 5 Diarias - ${todasAsDiarias.diaria5.toFixed(2)}`;
     const obj6 = document.querySelector('.infoDiaria6');
-    obj6.innerHTML = `6 Diaria - ${todasAsDiarias.diaria6.toFixed(2)}`;
+    obj6.innerHTML = `| 6 Diarias - ${todasAsDiarias.diaria6.toFixed(2)}`;
     const obj7 = document.querySelector('.infoDiaria7');
-    obj7.innerHTML = `7 Diaria - ${todasAsDiarias.diaria7.toFixed(2)}`;
+    obj7.innerHTML = `| 7 Diarias - ${todasAsDiarias.diaria7.toFixed(2)}`;
     const obj8 = document.querySelector('.infoDiaria8');
-    obj8.innerHTML = `8 Diaria - ${todasAsDiarias.diaria8.toFixed(2)}`;
+    obj8.innerHTML = `| 8 Diarias - ${todasAsDiarias.diaria8.toFixed(2)}`;
     const obj9 = document.querySelector('.infoDiaria9');
-    obj9.innerHTML = `9 Diaria - ${todasAsDiarias.diaria9.toFixed(2)}`;
+    obj9.innerHTML = `| 9 Diarias - ${todasAsDiarias.diaria9.toFixed(2)}`;
     const obj10 = document.querySelector('.infoDiaria10');
-    obj10.innerHTML = `10 Diaria - ${todasAsDiarias.diaria10.toFixed(2)}`;
+    obj10.innerHTML = `| 10 Diarias - ${todasAsDiarias.diaria10.toFixed(2)}`;
 }
 newFunction();
